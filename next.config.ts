b0c31cf0091d3640fa14e646c18cc2ec3b7c8687
EDUARDO EFRAIN GARCIA SUAREZ <egarcia1415@mich.conalep.edu.mx>
2024-11-13 19:12:05 -0600

    next Config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+// Configuración para GitHub Actions
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  assetPrefix,
+  basePath,
+  output: "export",
   images: {
     remotePatterns: [
       {
