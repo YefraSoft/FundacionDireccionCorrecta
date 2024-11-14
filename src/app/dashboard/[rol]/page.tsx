7f8b5031ee0ae91a7d264aef48ad31e8323d0225
EDUARDO EFRAIN GARCIA SUAREZ <egarcia1415@mich.conalep.edu.mx>
2024-11-13 19:04:11 -0600

    change dasboard delete Rol nav

--- src/app/dashboard/[rol]/page.tsx
+++ src/app/dashboard/[rol]/page.tsx
@@ -1,18 +1,12 @@
 import DashbardCompany from "@/components/dashboards/companyDash";
 import { dashboardProps } from "@/utils/interfaces";
 import { notFound } from "next/navigation";
-import { use } from "react";
+import React from "react";
 
-export async function generateStaticParams() {
-  const roles = ["sponsor", "branch", "company"];
 
-  return roles.map((rol) => ({
-    params: { rol },
-  }));
-}
 
 export default function Dashbard({ params }: dashboardProps) {
-  const { rol } = use(params);
+  const { rol } = React.use(params);
 
   if (!rol || !["sponsor", "branch", "company"].includes(rol)) {
     notFound();
